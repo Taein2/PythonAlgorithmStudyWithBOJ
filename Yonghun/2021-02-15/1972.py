9be181266763aeb29a923664fb355a2a5394f54e
TwinkleRing <TwinkleRing@github.com>
2021-02-15 16:07:34 +0900

    2021-02-15

--- Yonghun/2021-02-15/1972.py
+++ Yonghun/2021-02-15/1972.py
@@ -3,17 +3,17 @@ while True:
     if s == '*':
         break
     
-    result = True
+    Flag = True
     for i in range(1, len(s)):
         word_list = []
         for j in range(len(s)-i):
             word = s[j] + s[j+i]
             word_list.append(word)
         if len(word_list) != len(set(word_list)):
-            result = False
+            Flag = False
             break
             
-    if result == True:
-        print("{} is surprising.".format(s))
+    if Flag == True:
+        print(f'{s} is surprising.')
     else:
-        print("{} is NOT surprising.".format(s))
+        print(f'{s} is NOT surprising.')
