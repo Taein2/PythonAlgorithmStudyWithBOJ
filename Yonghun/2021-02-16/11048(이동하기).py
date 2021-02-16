204aaa2d3db7c8b976af4035e29ec3aaf3e5776b
YoungHun Kim <dlrmwl15@gmail.com>
2021-02-16 22:17:12 +0900

    Update 11048(이동하기).py

--- Yonghun/2021-02-16/11048(이동하기).py
+++ Yonghun/2021-02-16/11048(이동하기).py
@@ -1,9 +1,10 @@
 n, m = map(int,input().split())
 array = [list(map(int,input().split())) for _ in range(n)]
 D = [[0] * (m + 1) for _ in range(n + 1)]
-for i in range(1, n + 1) :
-    for j in range(1, m + 1) :
-        D[i][j] = array[i-1][j-1] + max(D[i-1][j], D[i][j-1], D[i-1][j-1])
 
+for i in range(0, n) :
+    for j in range(0, m) :
+        D[i][j] = max(D[i-1][j], D[i][j-1])
+        D[i][j] += array[i][j]
 
-print(D[n][m]) 
+print(D[n-1][m-1])
