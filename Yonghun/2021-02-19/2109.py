7ec80362134da38a0595f3e0c0115b7ddf02731f
TwinkleRing <TwinkleRing@github.com>
2021-02-20 15:52:23 +0900

    2021-02-20

--- Yonghun/2021-02-19/2109.py
+++ Yonghun/2021-02-19/2109.py
@@ -1,1 +1,13 @@
 # 순회강연
+import heapq
+n = int(input())
+array = [list(map(int,input().split())) for _ in range(n)]
+array.sort(key = lambda x : x[1]) # 데드라인을 기준으로 정렬.
+
+sums = []
+for num in array :
+    heapq.heappush(sums, num[0])
+    if len(sums) > num[1] :
+        heapq.heappop(sums)
+
+print(sum(sums))
