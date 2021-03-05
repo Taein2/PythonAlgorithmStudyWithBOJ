00c4d53232f40481ff87575afed5402155f84757
TwinkleRing <TwinkleRing@github.com>
2021-03-05 13:34:20 +0900

    2021-03-05

--- Yonghun/2021-03-04/11058.py
+++ Yonghun/2021-03-04/11058.py
@@ -1,2 +1,15 @@
 # 크리보드(키보드)
 
+n = int(input())
+
+dp = [0 for i in range(101)]
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+for i in range(3, 101):
+    dp[i] = dp[i - 1] + 1
+    for j in range(i - 3, -1, -1):
+        dp[i] = max(dp[i], dp[j] * (i - j - 1))
+
+print(dp[n])
